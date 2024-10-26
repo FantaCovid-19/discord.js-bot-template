381f37cc6a95a5e303b719ae32263296e5e0b1a8
FantaCovid-19 <contact@mrfantasma.com>
2024-10-26 01:06:21 -0300

    feat: 🔥 Add Http Helpers
    Signed-off-by: MrFantasma <contact@mrfantasma.com>

---
+++ src/helpers/HttpUtils.js
@@ -0,0 +1,27 @@
+module.exports = class HttpUtils {
+  /**
+   * Make a GET request to a URL and return the response.
+   *
+   * @param {String} url - The URL to make the request to.
+   * @param {Object} [options] - The fetch options.
+   * @returns {Promise<Object>}
+   */
+  static async getJson(url, options) {
+    try {
+      const response = options ? await fetch(url, options) : await fetch(url);
+      const json = await response.json();
+
+      return {
+        success: response.status === 200 ? true : false,
+        status: response.status,
+        data: json,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        status: 500,
+        data: error,
+      };
+    }
+  }
+};
