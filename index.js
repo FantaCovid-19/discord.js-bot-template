57f9d35a55517c5560d292602723cf9d3f999ddb
FantaCovid-19 <contact@mrfantasma.com>
2024-10-27 12:24:20 -0300

    feat: 🔥 Add validation config and update check
    Signed-off-by: MrFantasma <contact@mrfantasma.com>

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 require('module-alias/register');
 
 const Client = require('@structures/BotClient');
+const { validateConfigurations } = require('@helpers/Validator');
+const { checkForUpdates } = require('@helpers/BotUtils');
+const config = require('@config');
 
 const client = new Client();
 
+validateConfigurations();
+
 (async () => {
+  if (config.checkUpdates) await checkForUpdates(process.env.GITHUB_REPO);
+
   await client.initialize();
 })();
